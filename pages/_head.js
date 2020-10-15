1b7b6ccbca0761a4bdd667e3d11ba9db17ae67ca
Aaron Cawte <aaron@bbncreative.co>
2020-10-15 23:01:48 +0100

    Enable PWA with next-pwa

--- pages/_head.js
+++ pages/_head.js
@@ -3,6 +3,20 @@ import PropTypes from "prop-types";
 
 const CustomHead = ({ title }) => (
   <Head>
+    <meta charset="utf-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+    />
+    <meta
+      name="description"
+      content="Words and work from Aaron Cawte, a front-end developer and product designer based in London, UK."
+    />
+    <meta
+      name="keywords"
+      content="blog,words,work,product,design,developer,web,london,uk"
+    />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
