24fcce274d12d6019c9908d54d53e1e0d2fdc6c1
Aaron Cawte <aaron.cawte@octoenergy.com>
2021-12-29 19:59:50 +0000

    Add prop-types to Container, remove redundant React imports

--- components/Container/index.jsx
+++ components/Container/index.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import PropTypes from "prop-types";
 import styles from "./Container.module.css";
 
 const Container = ({ children }) => (
   <div className={styles.Container}>{children}</div>
 );
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Container;
