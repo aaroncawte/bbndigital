01bbd11f7a291781cfff8eec438ede5d10b4c6e1
Aaron Cawte <6383947+aaroncawte@users.noreply.github.com>
2023-01-22 18:03:57 +0000

    Properly configure linebreak rules for multi-platform development

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ["@krakentech/eslint-config", "plugin:@next/next/recommended"],
+  rules: {
+    "linebreak-style": 0,
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
 };
