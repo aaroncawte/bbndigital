f6f2b3f278e89aba0368269266a182878636de3c
Aaron Cawte <6383947+aaroncawte@users.noreply.github.com>
2023-01-23 00:08:32 +0000

    Configure KT linting config and related dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-    React: "writable",
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  plugins: ["react", "prettier"],
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
-    "react/react-in-jsx-scope": "off",
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+  extends: ["@krakentech/eslint-config", "plugin:@next/next/recommended"],
 };
