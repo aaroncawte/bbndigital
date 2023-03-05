0f6246f93ddac81af6c5328f4f4185d7f56da58c
Aaron Cawte <6383947+aaroncawte@users.noreply.github.com>
2023-03-05 22:24:15 +0000

    Create Callout component

---
+++ components/Callout/Callout.styled.ts
@@ -0,0 +1,51 @@
+import styled, { css } from "styled-components";
+
+export enum CalloutVariants {
+  OFFBLACK = "offBlack",
+  PINK = "pink",
+  PURPLE = "purple",
+  BLUE = "blue",
+  YELLOW = "yellow",
+}
+
+export const CalloutStyled = styled.div<{ variant: CalloutVariants }>`
+  position: relative;
+  display: block;
+  width: 100%;
+  padding: 0.8rem 1.6rem 0.8rem;
+  margin: 2.4rem 0;
+  box-sizing: border-box;
+  border-width: 0;
+  border-style: solid;
+  border-top-width: 2.4rem;
+  border-radius: 0.4rem;
+
+  @media (min-width: 48rem) {
+    border-top-width: 0;
+    border-left-width: 2.4rem;
+  }
+
+  ${({ theme, variant }) => css`
+    border-color: ${theme.colors[variant]};
+    background-color: ${theme.wash[variant]};
+
+    a:hover,
+    a:focus {
+      color: ${theme.colors[variant]};
+      text-decoration-color: ${theme.colors[variant]};
+    }
+  `};
+`;
+
+export const CalloutIcon = styled.img`
+  position: absolute;
+  left: calc(50% - 0.8rem);
+  top: -2rem;
+  width: 1.6rem;
+  height: 1.6rem;
+
+  @media (min-width: 48rem) {
+    top: calc(50% - 0.8rem);
+    left: -2rem;
+  }
+`;
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,8 +1,25 @@
 export const theme = {
   colors: {
+    offBlack: "#161616",
     grey2: "#445",
     grey3: "#161616",
     pink: "#e8185f",
     purple: "#a900ff",
+    blue: "#08a4bd",
+    yellow: "#f9cb40",
+  },
+  tints: {
+    offBlack: "#161616",
+    pink: "hsl(340deg 82% 20%)",
+    purple: "hsl(280deg 100% 20%)",
+    blue: "hsl(188deg 92% 20%)",
+    yellow: "hsl(45deg 94% 20%)",
+  },
+  wash: {
+    offBlack: "hsl(0deg 0% 95%)",
+    pink: "hsl(340deg 82% 97%)",
+    purple: "hsl(280deg 100% 95%)",
+    blue: "hsl(188deg 92% 92%)",
+    yellow: "hsl(45deg 94% 95%)",
   },
 };
