52109e864a1bd45f40ef5d32503df2369da8e4a2
Aaron Cawte <6383947+aaroncawte@users.noreply.github.com>
2023-03-12 17:00:43 +0000

    Create non-indexed Design System page

--- utils/getAllPostsManually.ts
+++ utils/getAllPostsManually.ts
@@ -1,6 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { meta as metaDesignSystem } from "../pages/posts/design-system.mdx";
+
 export function getAllPostsManually({ indexable = false }) {
   if (indexable) {
-    return [].filter((post) => !post.noIndex);
+    return [metaDesignSystem].filter((post) => !post.noIndex);
   }
-  return [];
+  return [metaDesignSystem];
 }
