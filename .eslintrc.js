e38b4e46c392204ebffc7b3e051c955492c5c96a
Aaron Cawte <6383947+aaroncawte@users.noreply.github.com>
2023-06-18 15:11:40 +0100

    Ignore ban-ts-comment rule in ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["@krakentech/eslint-config", "plugin:@next/next/recommended"],
   rules: {
+    "@typescript-eslint/ban-ts-comment": 0,
     "linebreak-style": 0,
     "prettier/prettier": [
       "error",
