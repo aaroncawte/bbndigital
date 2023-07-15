f97c86985570ee1c8d91cb84066ebade8e15f816
Aaron Cawte <6383947+aaroncawte@users.noreply.github.com>
2023-07-15 16:01:05 +0100

    Show subheading and reading time in PostLayout

---
+++ components/PostLayout/PostLayout.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledSubheading = styled.div`
+  margin: -1.6rem 0 1.6rem;
+  padding: 0;
+  font-style: italic;
+  font-size: 1.6rem;
+  font-weight: 500;
+  color: ${({ theme }) => theme.colors.pink};
+`;
