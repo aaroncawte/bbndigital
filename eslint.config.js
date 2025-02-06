d9db24b6f1074f4b8d3f7f4d2779ae76efe3d7d6
Aaron Cawte <6383947+aaroncawte@users.noreply.github.com>
2025-02-06 10:06:49 +0000

    Update eslint and related configs

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+const linters = require("@krakentech/eslint-config");
+
+module.exports = linters.load(
+  {
+    ignores: ["**/node_modules/**", "public/", ".next/"],
+  },
+  {
+    files: ["**/*.{js,jsx,cjs,mjs}"],
+    extends: linters.configs.react.recommended,
+  },
+  {
+    files: ["**/*.{ts,tsx,mts,cts}"],
+    extends: linters.configs.typescript.recommended,
+    rules: {
+      // TODO: Remove these overrides once violations are resolved
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+    },
+  }
+);
