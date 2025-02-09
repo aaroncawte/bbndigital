f005270261841cff2708ab80548ac88e3e1abcfb
Aaron Cawte <6383947+aaroncawte@users.noreply.github.com>
2025-02-09 14:18:23 +0000

    Remove dependency on styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,13 @@
 import { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-import { ThemeProvider } from "styled-components";
-import StyledComponentsRegistry from "../lib/registry";
-import { theme } from "../styles/theme";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <StyledComponentsRegistry>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-        <Analytics />
-      </ThemeProvider>
-    </StyledComponentsRegistry>
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
   );
 }
 
