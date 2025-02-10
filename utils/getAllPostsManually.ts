3d1b58ea27194e2a612ee756aa78d5d70a4039b6
Aaron Cawte <6383947+aaroncawte@users.noreply.github.com>
2025-02-10 12:02:16 +0000

    Define type for post metadata object

--- utils/getAllPostsManually.ts
+++ utils/getAllPostsManually.ts
@@ -5,11 +5,25 @@ import { meta as metaCwcFunnel } from "../pages/posts/cwc-funnel.mdx";
 // @ts-expect-error: mdx export issue
 import { meta as metaDesignSystem } from "../pages/posts/design-system.mdx";
 
-export function getAllPostsManually({ indexable = false }) {
+interface PostMetadata {
+  title: string;
+  description: string;
+  publishedAt: string | undefined;
+  ogImage: string | undefined;
+  slug: string;
+  wordCount: number | undefined;
+  noIndex: boolean | undefined;
+}
+
+export function getAllPostsManually({ indexable = false }): PostMetadata[] {
   if (indexable) {
     return [metaCwcFunnel, metaContendingWithChaos, metaDesignSystem].filter(
-      (post) => !post.noIndex,
-    );
+      (post: PostMetadata) => !post.noIndex,
+    ) as PostMetadata[];
   }
-  return [metaCwcFunnel, metaContendingWithChaos, metaDesignSystem];
+  return [
+    metaCwcFunnel,
+    metaContendingWithChaos,
+    metaDesignSystem,
+  ] as PostMetadata[];
 }
